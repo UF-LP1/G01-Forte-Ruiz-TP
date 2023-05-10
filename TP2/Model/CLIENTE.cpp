a0f397b0a743030f37bbe3879f6573d0854c7d3c
Isabella Forte <isabellaannaforte@gmail.com>
2023-05-09 22:00:14 -0300

    Cambio cliente .h y .cpp

--- TP2/Model/CLIENTE.cpp
+++ TP2/Model/CLIENTE.cpp
@@ -2,52 +2,36 @@
 
 //constructor y destructor
 
-CLIENTE::CLIENTE(vector<string> alias_productos, unsigned int numero, bool tiene_jpg, string dni, string mail, bool quiere_ticket_impreso, FORMA_PAGO forma_pago, string alias_disfraz_alquilar, TALLE talle_disfraz_alquilar, bool quiere_disfraz, bool quiere_envolver_regalo, bool quiere_devolver_disfraz, string alias_disfraz_devuelve, TALLE talle_disfraz_devuelve)
+CLIENTE::CLIENTE(vector<LISTA_PR> productos_a_buscar, unsigned int numero, bool tiene_jpg, string dni, string mail, bool quiere_ticket_impreso, FORMA_PAGO forma_pago, string alias_disfraz_alquilar, TALLE talle_disfraz_alquilar, bool quiere_disfraz, bool quiere_devolver_disfraz, string alias_disfraz_devuelve, TALLE talle_disfraz_devuelve)
 {
-	this->alias_productos = alias_productos;
+	this->productos_a_buscar = productos_a_buscar;
 	this->numero = numero;
 	this->tiene_jpg = tiene_jpg;
 	this->dni = dni;
 	this->mail = mail;
+
 	this->quiere_ticket_impreso = quiere_ticket_impreso;
 	this->forma_pago = forma_pago;
+
 	this->alias_disfraz_alquilar = alias_disfraz_alquilar;
 	this->talle_disfraz_alquilar = talle_disfraz_alquilar;
 	this->quiere_disfraz = quiere_disfraz;
-	this->quiere_envolver_regalo = quiere_envolver_regalo;
+
 	this->quiere_devolver_disfraz = quiere_devolver_disfraz;
 	this->alias_disfraz_devuelve = alias_disfraz_devuelve;
 	this->talle_disfraz_devuelve = talle_disfraz_devuelve;
 
 }
-CLIENTE::CLIENTE() {
-	this->numero = 0;
-	this->tiene_jpg = false;
-	this->dni = '0';
-	this->mail = "nombre.mail.com";
-	this->quiere_ticket_impreso = false;
-	this->forma_pago = efectivo;
-	this->alias_disfraz_alquilar = "personaje";
-	this->talle_disfraz_alquilar = M;
-	this->quiere_disfraz = false;
-
-	this->quiere_envolver_regalo = false;
-	this->quiere_devolver_disfraz = false;
-	this->alias_disfraz_devuelve = "personaje";
-	this->talle_disfraz_devuelve = M;
-
-}
 CLIENTE::~CLIENTE()
 {
-	this->alias_productos.~vector();
-}
-
-void CLIENTE::set_alias_productos(vector<string> alias_productos)
-{
-	this->alias_productos = alias_productos;
 }
 
 //setters
+void CLIENTE::set_productos_a_buscar(vector<LISTA_PR> productos_a_buscar)
+{
+	this->productos_a_buscar = productos_a_buscar;
+}
+
 void CLIENTE::set_numero(unsigned int numero)
 {
 	this->numero = numero;
@@ -64,6 +48,7 @@ void CLIENTE::set_mail(string mail)
 {
 	this->mail = mail;
 }
+
 void CLIENTE::set_quiere_ticket_impreso(bool quiere_ticket_impreso)
 {
 	this->quiere_ticket_impreso = quiere_ticket_impreso;
@@ -72,6 +57,7 @@ void CLIENTE::set_forma_pago(FORMA_PAGO forma_pago)
 {
 	this->forma_pago = forma_pago;
 }
+
 void CLIENTE::set_alias_disfraz_alquilar(string alias_disfraz_alquilar)
 {
 	this->alias_disfraz_alquilar;
@@ -85,10 +71,6 @@ void CLIENTE::set_quiere_disfraz(bool quiere_disfraz)
 	this->quiere_disfraz = quiere_disfraz;
 }
 
-void CLIENTE::set_quiere_envolver_regalo(bool quiere_envolver_regalo)
-{
-	this->quiere_envolver_regalo = quiere_envolver_regalo;
-}
 void CLIENTE::set_quiere_devolver_disfraz(bool quiere_devolver_disfraz)
 {
 	this->quiere_devolver_disfraz = quiere_devolver_disfraz;
@@ -101,25 +83,14 @@ void CLIENTE::set_talle_disfraz_devuelve(TALLE talle_disfraz_devuelve)
 {
 	this->talle_disfraz_devuelve = talle_disfraz_devuelve;
 }
-JPG obtener_imagen(string URL, unsigned int ancho, unsigned int alto, bool a_color, float senia) {
-	JPG imagen(URL, ancho, alto, a_color, senia);
 
-	return imagen;
-}
-
-CARRITO obtener_carrito(vector<PRODUCTO> lista) {
-	CARRITO carrito(lista);
-	return carrito;
-
-}
-
-
-vector<string> CLIENTE::get_alias_productos()
-{
-	return this->alias_productos;
-}
 
 //getters
+vector<LISTA_PR> CLIENTE::get_productos_a_buscar()
+{
+	return this->productos_a_buscar;
+}
+
 unsigned int CLIENTE::get_numero()
 {
 	return this->numero;
@@ -136,6 +107,7 @@ string CLIENTE::get_mail()
 {
 	return this->mail;
 }
+
 bool CLIENTE::get_quiere_ticket_impreso()
 {
 	return this->quiere_ticket_impreso;
@@ -144,6 +116,7 @@ FORMA_PAGO CLIENTE::get_forma_pago()
 {
 	return this->forma_pago;
 }
+
 string CLIENTE::get_alias_disfraz_alquilar()
 {
 	return this->alias_disfraz_alquilar;
@@ -157,10 +130,6 @@ bool CLIENTE::get_quiere_disfraz()
 	return this->quiere_disfraz;
 }
 
-bool CLIENTE::get_quiere_envolver_regalo()
-{
-	return this->quiere_envolver_regalo;
-}
 bool CLIENTE::get_quiere_devolver_disfraz()
 {
 	return this->quiere_devolver_disfraz;
@@ -173,8 +142,13 @@ TALLE CLIENTE::get_talle_disfraz_devuelve()
 {
 	return this->talle_disfraz_devuelve;
 }
-
-void CLIENTE::agregar_a_carrito(PRODUCTO producto)
+//otros
+JPG CLIENTE::obtener_imagen(string URL, unsigned int ancho, unsigned int alto, bool a_color, float senia)
 {
-	
+	return JPG();
+}
+
+CARRITO CLIENTE::obtener_carrito(vector<PRODUCTO> lista)
+{
+	return CARRITO();
 }
--- TP2/Model/CLIENTE.h
+++ TP2/Model/CLIENTE.h
@@ -7,6 +7,7 @@
 #include "FORMA_PAGO.h"
 #include "JPG.h"
 #include "TALLE.h"
+#include "LISTA_PR.h"
 #include <vector>
 using namespace std;
 
@@ -14,67 +15,68 @@ class CLIENTE {
 public: 
     
 //constructor y destructor
-CLIENTE(vector<string> alias_productos,unsigned int numero, bool tiene_jpg, string dni, string mail, bool quiere_ticket_impreso, FORMA_PAGO forma_pago, string alias_disfraz_alquilar, TALLE talle_disfraz_alquilar, bool quiere_disfraz, bool quiere_envolver_regalo, bool quiere_devolver_disfraz, string alias_disfraz_devuelve, TALLE talle_disfraz_devuelve);
-CLIENTE();
+CLIENTE(vector<LISTA_PR> productos_a_buscar,unsigned int numero, bool tiene_jpg, string dni, string mail, bool quiere_ticket_impreso, FORMA_PAGO forma_pago, string alias_disfraz_alquilar, TALLE talle_disfraz_alquilar, bool quiere_disfraz, bool quiere_devolver_disfraz, string alias_disfraz_devuelve, TALLE talle_disfraz_devuelve);
+
 ~CLIENTE();
 
  //setters
-void set_alias_productos(vector<string> alias_productos);
+void set_productos_a_buscar(vector<LISTA_PR> productos_a_buscar);
 
 void set_numero(unsigned int numero);
 void set_tiene_jpg(bool tiene_jpg);
 void set_dni(string dni);
 void set_mail(string mail);
+
 void set_quiere_ticket_impreso(bool quiere_ticket_impreso);
 void set_forma_pago(FORMA_PAGO forma_pago);
+
 void set_alias_disfraz_alquilar(string alias_disfraz_alquilar);
 void set_talle_disfraz_alquilar(TALLE talle_disfraz_alquilar);
 void set_quiere_disfraz(bool quiere_disfraz);
 
-void set_quiere_envolver_regalo(bool quiere_envolver_regalo);
 void set_quiere_devolver_disfraz(bool quiere_devolver_disfraz);
 void set_alias_disfraz_devuelve(string alias_disfraz_devuelve);
 void set_talle_disfraz_devuelve(TALLE talle_disfraz_devuelve);
 
 
 //getters
-vector<string> get_alias_productos();
+vector<LISTA_PR> get_productos_a_buscar();
 
 unsigned int get_numero();
 bool get_tiene_jpg( );
 string get_dni( );
 string get_mail();
+
 bool get_quiere_ticket_impreso( );
 FORMA_PAGO get_forma_pago( );
+
 string get_alias_disfraz_alquilar( );
 TALLE get_talle_disfraz_alquilar( );
 bool get_quiere_disfraz( );
 
-bool get_quiere_envolver_regalo( );
 bool get_quiere_devolver_disfraz();
 string get_alias_disfraz_devuelve( );
 TALLE get_talle_disfraz_devuelve( );
 
-void agregar_a_carrito(PRODUCTO producto);
-
+//otros
 JPG obtener_imagen(string URL, unsigned int ancho, unsigned int alto, bool a_color, float senia);
- 
 CARRITO obtener_carrito(vector<PRODUCTO> lista);
  
 private: 
-    vector<string> alias_productos;
+    vector<LISTA_PR> productos_a_buscar;
    
     unsigned int numero;
     bool tiene_jpg;
     string dni;
     string mail;
+
     bool quiere_ticket_impreso;
     FORMA_PAGO forma_pago;
+
     string alias_disfraz_alquilar;
     TALLE talle_disfraz_alquilar;
     bool quiere_disfraz;
-  
-    bool quiere_envolver_regalo;
+ 
     bool quiere_devolver_disfraz;
     string alias_disfraz_devuelve;
     TALLE talle_disfraz_devuelve;
